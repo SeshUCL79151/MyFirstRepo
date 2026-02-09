7caf283ce5a1bdc50e661fd7cc57e6fa18950e5c
ŚeshUCL <sesh79151@edu.ucl.dk>
2026-02-09 23:15:15 +0100

    Added randomness, as well as comments

--- GaetEtTal.cs
+++ GaetEtTal.cs
@@ -6,11 +6,16 @@ namespace GaetEtTal
     {
         static void Main(string[] args)
         {
-            int tal = 7;
+            // Opret en instans af Random-klassen
+            Random random = new Random();
+
+            // Generer et tilfældigt tal mellem 1 og 10
+            int tal = random.Next(1, 11);
 
             Console.WriteLine("Du skal nu gætte et tal mellem 1 og 10!");
-            int gæt = Convert.ToInt32(Console.ReadLine());
+            int gæt = Convert.ToInt32(Console.ReadLine()); // Læs brugerens gæt
 
+            // Tjek om gættet er korrekt
             if (gæt == tal)
             {
                 Console.WriteLine("Tillykke, du gættede rigtigt!");
